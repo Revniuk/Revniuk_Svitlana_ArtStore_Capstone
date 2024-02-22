84cbd5bdb28240fac9e54b71fa3ffe431c888974
Svitlana Revniuk <69059791+Revniuk@users.noreply.github.com>
2024-02-22 13:19:50 -0500

    Add files via upload

---
+++ ArtStore/ArtStore/Server/app.js
@@ -0,0 +1,30 @@
+const artworkRouter = require('./routes/artwork');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+// Create Express server
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+// Define routes
+const artworkRouter = require('./routes/artwork');
+app.use('/artwork', artworkRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
